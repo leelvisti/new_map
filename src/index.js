d9214ab9942925be30c7acbed345bc80f84b61a1
Elvis Le <leelvisti@gmail.com>
2020-10-10 12:57:03 -0700

    added legend, banner, helper and hover interaction

--- src/index.js
+++ src/index.js
@@ -5,11 +5,10 @@ import settings from "./settings.json";
 import custom from "./custom-style.json";
 
 let map;
-// let hovered;
-// const popup = document.querySelector("popup");
+let hovered;
+const popup = document.querySelector("#popup");
 
 async function init() {
-    const sites = await import("../data/sites.json");
     const neighborhoods = await import("../data/output.json");
     const style = map.getStyle();
 
@@ -20,10 +19,89 @@ async function init() {
     style.layers.push(...custom.layers);
     map.setStyle(style);
 
-    map.getSource("sites").setData(sites);
     map.getSource("neighborhoods").setData(neighborhoods)
+
+    initPopup();
+    initLegend();
+    initHelper();
 }
 
 mapboxgl.accessToken = settings.accessToken;
 map = new mapboxgl.Map(settings);
 map.on("load", init);
+
+// initialize popup display about neighborhood when hovering
+const initPopup = () => {
+    const nameEl = popup.querySelector(".name");
+    const countEl = popup.querySelector(".count");
+
+    map.on("mousemove", "neighborhoods-fill", (e) => {
+        clearHover();
+        if (e.features.length > 0) {
+            hovered = e.features[0];
+            map.setFeatureState(hovered, {
+                hover: true
+            });
+            popup.style.display = "block";
+            nameEl.textContent = hovered.properties.name;
+            countEl.textContent = hovered.properties.count;
+        }
+    });
+
+    map.on("mouseleave", "neighborhoods-fill", clearHover);
+}
+
+// remove hover information
+const clearHover = () => {
+    if (hovered) {
+        map.setFeatureState(hovered, {
+            hover: false
+        });
+
+        hovered = null;
+    }
+
+    popup.style.display = "none";
+}
+
+// populate legend
+const initLegend = () => {
+    const legend = document.querySelector("#legend");
+    const template = document.querySelector("#legend-entry");
+    const fillColorStyle = map.getPaintProperty("neighborhoods-fill", "fill-extrusion-color");
+    
+    let total = 0;
+
+    fillColorStyle.splice(0, 2);
+
+    for (let i = 0; i < fillColorStyle.length; i += 2) {
+        const entry = document.importNode(template.content, true);
+        const spans = entry.querySelectorAll("span");
+        const color = fillColorStyle[i];
+
+        spans[0].style.backgroundColor = color;
+
+        if (i === fillColorStyle.length - 1) {
+            spans[1].textContent = `>=${total}`;
+        } else {
+            spans[1].textContent = `${total}-${fillColorStyle[i + 1]}`;
+            total = fillColorStyle[i + 1];
+        }
+
+        legend.appendChild(entry);
+    }
+}
+
+// initialize tooltip about how to manipulate map
+const initHelper = () => {
+    const helper = document.querySelector("#helper");
+    const helperSpans = helper.querySelectorAll("span");
+    const helperIcon = helperSpans[0];
+
+    helperIcon.onclick = () => {
+        const helperState = helperSpans[1].style.visibility;
+        helperSpans[1].style.visibility = helperState === 'visible'
+            ? 'hidden'
+            : 'visible';
+    }
+}
